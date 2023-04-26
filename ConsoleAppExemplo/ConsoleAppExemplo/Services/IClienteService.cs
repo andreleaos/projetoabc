9a5f0c75cc52398110b4a03833a9fed73647416b
andreleaos <andreleaos@gmail.com>
2023-04-25 21:42:40 -0300

    definicao das interfaces

---
+++ ConsoleAppExemplo/ConsoleAppExemplo/Services/IClienteService.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleAppExemplo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExemplo.Services
+{
+    public interface IClienteService
+    {
+        void Add(Cliente cliente);
+        void Update(Cliente cliente);
+        void Delete(int id);
+        List<Cliente> GetAll();
+        Cliente Get(int id);
+    }
+}
